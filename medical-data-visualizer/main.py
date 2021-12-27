0caba7bebdf47e5a7e23e8f5661cf95971bc9ad4
borntofrappe <borntofrappe@gmail.com>
2021-12-27 18:44:43 +0100

    complete assignment

--- medical-data-visualizer/main.py
+++ medical-data-visualizer/main.py
@@ -3,35 +3,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Import data
 df = pd.read_csv('medical_examination.csv')
 
+# Add 'overweight' column
 # BMI = weight / height ^ 2 = kg / m ^ 2
 # height is in centimeters
-df['overweight'] = df['weight'] / (df['height'] / 100) ** 2 > 25
+# Normalize False and True to 0 and 1; technically unnecessary as booleans are interpreted with the corresponding integer values
+df['overweight'] = (df['weight'] / (df['height'] / 100) **
+                    2 > 25).replace({True: 1, False: 0})
 
-# map False to 0, map True to 1 (superfluous since booleans are interpreted with the corresponding integer values)
-df['overweight'] = df['overweight'].replace({True: 1, False: 0})
-
-# map 1 to 0, map greater than 1 to 1
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df[['cholesterol', 'gluc']] = df[['cholesterol', 'gluc']].astype(
     str).replace({'1': 0, '[2-9]': 1}, regex=True)
 
 
 def draw_cat_plot():
-    # long format studying the 'active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke' columns per the 'cardio' value
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df, id_vars=['cardio'], value_vars=[
                      'active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
-    fig = sns.catplot(data=df_cat, kind='count',
-                      col='cardio', x='variable', hue='value')
-    fig.set(ylabel='total')
+    # Draw the catplot with 'sns.catplot()'
+    # .set(ylabel) and .fig necessary to pass the unit tests @freecodecamp
+    # technically catplot returns a FacetGrid comopsed of the figure
+    fig = sns.catplot(data=df_cat, kind='count', col='cardio',
+                      x='variable', hue='value').set(ylabel='total').fig
 
+    # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
 
 def draw_heat_map():
-    # drop invalid data following the specs
+    # Clean the data
     df_heat = df.drop(df[
         (df['ap_lo'] > df['ap_hi']) |
         (df['height'] < df['height'].quantile(0.025)) |
@@ -40,16 +44,21 @@ def draw_heat_map():
         (df['weight'] > df['weight'].quantile(0.975))
     ].index)
 
+    # Calculate the correlation matrix
     corr = df_heat.corr()
 
+    # Generate a mask for the upper triangle
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)] = 1
 
+    # Set up the matplotlib figure
     fig, ax = plt.subplots(figsize=(9, 8))
 
+    # Draw the heatmap with 'sns.heatmap()'
     sns.heatmap(corr, annot=True, annot_kws={
                 'fontsize': 'small'}, fmt='.1f', mask=mask, linewidths=0.5, cbar_kws={'shrink': 0.5}, square=True)
 
+    # Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fig
 
